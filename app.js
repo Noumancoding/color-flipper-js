8a13c36ea1ad82abb633d295e6a80b3b7c78cb06
Nouman Coding <noumanali.coding@gmail.com>
2025-07-10 16:50:06 +0500

    feat: add array of color options

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // will handle color flipping logic
 const btn = document.getElementById("flipBtn")
 const colorText = document.getElementById("color-value")
+
+const colors = [
+  "#f44336",
+  "#2196f3",
+  "#4caf50",
+  "#ff9800",
+  "#9c27b0"
+]
