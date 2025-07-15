521bf15f07666bd4125480143226d8ba37531369
Nouman Coding <noumanali.coding@gmail.com>
2025-07-15 12:44:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // will handle color flipping logic
-const btn = document.getElementById("flipBtn")
-const colorText = document.getElementById("color-value")
+const btn = document.getElementById("flipBtn");
+const colorText = document.getElementById("color-value");
 
 const colors = [
   "#f44336",
@@ -8,12 +8,12 @@ const colors = [
   "#4caf50",
   "#ff9800",
   "#9c27b0"
-]
+];
 
 btn.addEventListener("click", () => {
-  const index = Math.floor(Math.random() * colors.length)
-  const selectedColor = colors[index]
-
-  document.body.style.backgroundColor = selectedColor
-  colorText.textContent = selectedColor
-})
+  const index = Math.floor(Math.random() * colors.length);
+  const selectedColor = colors[index];
+  
+  document.body.style.backgroundColor = selectedColor;
+  colorText.textContent = selectedColor;
+});
